67cc1790168b3eb2e65249cd9401caac135fef65
rojash03 <np05cp4a240283@iic.edu.np>
2025-06-23 11:26:19 +0545

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function add (){
+    let a =10;
+    let b = 20;
+    console. log(a+b);
+}
+add();
+
+const arrow = () => {
+    let a = 10;
+    let b = 20;
+    console.log(a + b);
+}
+arrow();
