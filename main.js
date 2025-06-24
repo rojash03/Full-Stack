c8d27138fef38e4540c0917d32870994e7181bcc
rojash03 <np05cp4a240283@iic.edu.np>
2025-06-24 11:04:48 +0545

    third commit

--- main.js
+++ main.js
@@ -73,3 +73,72 @@ if (averagemarks >= 40) {
 } else {
     console.log("fail");
 }
+
+let student = {
+    student1: {
+        name: "Ram",
+        class: "1",
+        marks: {
+            math: 95,
+            science: 90,
+            english: 85
+        }
+    },    
+    student2: {
+        name: "shyam",
+        class: "1",
+        marks: {
+            math: 95,
+            science: 90,
+            english: 85
+        }
+    },
+    student3: {
+        name: "hari",
+        class: "1",
+        marks: {
+            math: 95,
+            science: 90,
+            english: 85
+        }
+    }
+}
+for (let stud in student) {
+    console.log(stud, student[stud]);
+}
+
+for (let stud in student) {
+    console.log(stud, student[stud].marks);
+}
+let totalscore = 0;
+let studentnumber = 0;
+for (let stud in student) {
+    for (let marks in student[stud].marks) {
+        totalscore = student[stud].marks[marks] + totalscore;
+        studentnumber++;
+    }
+    console.log("Student name: " + student[stud].name);
+    console.log("Total score: " + totalscore);
+
+    let average = totalscore / studentnumber;
+    console.log("Average score: " + average);
+
+    if (average >= 40) {
+    console.log("You are passed");
+    } else {
+        console.log("Fail");
+}
+}
+
+let classtotalmarks = 0;
+let classstudentnumber = 0;
+for (let stud in student) {
+    for (let marks in student[stud].marks) {
+        classtotalmarks = student[stud].marks[marks] + classtotalmarks;
+        classstudentnumber++;
+    }
+}
+
+let classaverage = classtotalmarks / classstudentnumber;
+console.log("Class average score: " + classaverage);
+console.log("Class total marks: " + classtotalmarks);
